436e943cbfacf934f952c72a4b633f4ebdfa636d
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-07-03 12:59:02 +0530

    Added : Check analyze mood

--- main/java/com/bridzelab/MoodAnalyzer.java
+++ main/java/com/bridzelab/MoodAnalyzer.java
@@ -2,4 +2,10 @@ package com.bridzelab;
 
 public class MoodAnalyzer {
 
+    public String analyzeMood(String message) {
+        if (message.contains("Iam in Sad Mood"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
 }
